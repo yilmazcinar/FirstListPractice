a95db2fa6b15ef880648cb3fb38a671a6d21b6f3
yilmazcinar <cinaryilmaz96@gmail.com>
2025-01-19 19:25:14 +0300

    Add files via upload

---
+++ FirstListPractice/FirstListPractice/Program.cs
@@ -0,0 +1,29 @@
+﻿List <string> guest = new List<string>();
+
+Console.WriteLine("Galaya hoşgeldiniz. \n");
+
+while (true)
+{
+    Console.WriteLine("\nZiyaretçi ismi giriniz. Bitirmek için 'bitir' yazın. \n\n");
+    string isim = Console.ReadLine();
+
+    
+    
+    if (isim.ToLower() == "bitir")
+    {
+        break;
+
+    }
+
+    guest.Add(isim);
+
+
+
+}
+
+Console.WriteLine("\nGuest List : \n");
+
+foreach (var item in guest)
+{
+    Console.WriteLine($"{item}\n");
+}
